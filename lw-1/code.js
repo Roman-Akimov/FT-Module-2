f922afc424006e1fc219ca6a18998dbbd023cf55
Roman-Akimov <roma_akimov_12@mail.ru>
2025-02-08 23:27:40 +0400

    lw 1 is completed

---
+++ lw-1/code.js
@@ -0,0 +1,32 @@
+alert(
+  "Вариант 2. Задание: Напишите исходный код программы, которая выводит в консоль все автоморфные числа на заданном пользователем отрезке [a, b]. Натуральное число называется автоморфным, если десятичная запись его квадрата оканчивается на десятичную запись этого числа."
+);
+
+// Получим границы
+let leftBorder = Number(prompt("Введите левую границу: "));
+let rightBorder = Number(prompt("Введите правую границу: "));
+
+if (isNaN(leftBorder) || isNaN(rightBorder)) {
+  alert("Ошибка! Пожалуйста, введите корректные числа.");
+} 
+
+else {
+  function Authomorphic(num) {
+    let square = num * num;
+    return square.toString().endsWith(num.toString());
+  }
+
+  let Authomorphic_Num = [];
+
+  for (let i = leftBorder; i <= rightBorder; i++) {
+    if (Authomorphic(i)) {
+      Authomorphic_Num.push(i);
+    }
+  }
+
+  if (Authomorphic_Num.length > 0) {
+    alert("Автоморфные числа: " + Authomorphic_Num.join(", "));
+  } else {
+    alert("Нет автоморфных чисел в этом интервале.");
+  }
+}
